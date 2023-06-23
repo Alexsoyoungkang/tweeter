f23cd7400a7a8b09f0c52c323be50553db4d2774
Soyoung <ksyoung3424@gmail.com>
2023-06-23 20:54:03 +0000

    Fetching tweets with Ajax

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,36 +4,7 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// fake database
-// contains an array of tweet objects, where each object represents
-// a tweet and includes properties like the user's name, avatar and etc
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1687307355974
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1687393755974
-  }
-];
-
-
-// Document Ready Event
+// Document Ready Event //
 // waits for the document to be fully loaded and ready before executing any JavaScript.
 // The data from the tweetObj is dynamically inserted into the appropriate places using ${} notation.
 $(document).ready(function() {
@@ -51,7 +22,7 @@ $(document).ready(function() {
           <p>${tweetObj.content.text}</p>
         </div>
         <footer>
-          <span>${tweetObj.created_at}</span>
+          <span>${timeago.format(tweetObj.created_at)}</span>
           <span>
             <i class="fa-solid fa-flag"></i>
             <i class="fa-solid fa-retweet"></i>
@@ -64,26 +35,34 @@ $(document).ready(function() {
     return $tweet; //This jQuery object represents the entire tweet element structure
   };
 
+  // Render Tweets Function //
   const renderTweets = function(tweets) { // takes an array of tweet objects as input
     for (const tweet of tweets) { // loops through tweets
       const returnValue = createTweetElement(tweet); // calls createTweetElement for each tweet
-      $('#tweets-container').append(returnValue);  // takes return value and appends it to the tweets container
+      $('#tweets-container').prepend(returnValue);  // takes return value and appends it to the tweets container
     }
   };
   
-  renderTweets(data);
+  // Load Tweets Function //
+  const loadTweets = function() {
+    $.ajax("http://localhost:8080/tweets", { method: 'GET'}) // send GET request to this URL
+      .then(function(tweets) { // successful response from the server. the 'tweets' parameter = response data retrived from the server aka the array of tweets
+        renderTweets(tweets); // pass the tweets data to render the tweets on the page
+      });
+  };
+  loadTweets();
 
-  // Event listener for form submit
+  // Event Listener For Form Submit //
   $('form').on('submit', function(event) {
     event.preventDefault(); // Prevent default form submission behavior
     const data = $(this).serialize(); // Serialize form data
     console.log("data:", data);
 
-    $.ajax({
+    $.ajax({ // sends POST request to the server using Ajax
       type: 'POST',
-      data: data,
-      url: "http://localhost:8080/tweets",
-      success: function(res) { // method
+      data: data, // serialized form data above
+      url: "http://localhost:8080/tweets", // the server endpoint where the data should be sent
+      success: function(res) { // method -  callback functions to handle the response from the server.
         console.log(res);
       },
       error: function(error) {
